06816bd44fce93816dc5e227fcf3ff36740c6503
khatrabiru <khatrabiru@gmail.com>
2022-01-06 12:56:22 +0000

    Allow word in upplercase also set header to all type of website.

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ app.set('view engine', 'ejs');
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://fast-shore-86089.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
