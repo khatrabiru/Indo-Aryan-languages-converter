952e54007b8cade8e81e699b41492a462b4f6a7d
khatrabiru <khatrabiru@gmail.com>
2022-01-10 16:28:05 +0000

    Addhistory to Nepali language page.

--- client/src/components/languages/nepali.js
+++ client/src/components/languages/nepali.js
@@ -4,19 +4,44 @@ const Nepali = () => {
         <div>
             <div>
                 <p>
-                Nepali language, also called Gurkha, Gorkhali, Gurkhali, 
-                or Khaskura, member of the Pahari subgroup of the Indo-Aryan 
-                group of the Indo-Iranian division of the Indo-European 
-                languages. Nepali is spoken by more than 17 million people, 
-                mostly in Nepal, neighbouring parts of India and about a 
-                quarter of the population in Bhutan.
-                The origin of modern Nepali language is believed to be from 
-                Sinja valley of Jumla. Historically, the language was only spoken 
-                by the Khas people of Karnali Region. An archaic dialect of the 
-                language is spoken in Karnali.
+                    Nepali language, also called Gurkha, Gorkhali, Gurkhali,
+                    or Khaskura, member of the Pahari subgroup of the Indo-Aryan
+                    group of the Indo-Iranian division of the Indo-European
+                    languages. Nepali is spoken by more than 17 million people,
+                    mostly in Nepal, neighbouring parts of India and about a
+                    quarter of the population in Bhutan.
+                    The origin of modern Nepali language is believed to be from
+                    Sinja valley of Jumla. Historically, the language was only spoken
+                    by the Khas people of Karnali Region. An archaic dialect of the
+                    language is spoken in Karnali.
                 </p>
             </div>
-           
+            <div>
+                <p>
+                    Nepali is said to have originated from a now extinct language called
+                    'Khas Kura' or the Khas talk. The Khas people were ancient war tribes
+                    living in what we now call Uttarakhand in India and Western Nepal.
+                    According to Mahabharata, the Khasas are mentioned as barbaric tribes
+                    fighting for the Kaurava army. The modern Nepali language has
+                    numerous features and words which suggest that the language is
+                    ancient.
+
+                    Since the creation of greater Nepal, the Nepali language has
+                    been popular as an easy means of communication between all
+                    language speakers of the nation. Many years before the creation
+                    of greater Nepal, the Nepali language had been in use from
+                    Bhramhaputra in the east and Kashmir in the west. The linguists
+                    have taken the stone inscription of Adityabanshi king Damupal,
+                    dating back to 1038 BS, as the official source of Nepali language.
+                    A copperplate inscription dating back to 1280 BS is the historical
+                    vidence of Krachalya Kings victory over Kedar-land Gadawal to create
+                    Sija's Rigime. After studying the gold inscription of Prithvi Malla
+                    of 1413 B.S, we can surmise that the Nepali language came into
+                    existence in Gorkha and Gadawal since then.
+
+                </p>
+            </div>
+
         </div>
     );
 
