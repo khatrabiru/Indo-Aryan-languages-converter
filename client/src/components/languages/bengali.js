a86601ce855c44e042a89a23c4daae7c907b2732
khatrabiru <khatrabiru@gmail.com>
2022-01-11 17:24:22 +0000

    Add basic info on Bengali language and it's history.

--- client/src/components/languages/bengali.js
+++ client/src/components/languages/bengali.js
@@ -4,12 +4,38 @@ const Bengali = () => {
         <div>
             <div>
                 <p>
-
+                    Bengali language, Bengali Bangla, member of the Indo-Aryan group of the Indo-Iranian branch
+                    of the Indo-European language family. It is spoken by more than 210 million people as a firsts
+                    or second language, with some 100 million Bengali speakers in Bangladesh; about 85 million in
+                    India, primarily in the states of West Bengal, Assam, and Tripura; and sizable immigrant
+                    communities in the United Kingdom, the United States, and the Middle East. It is the state
+                    language of Bangladesh and one of the languages officially recognized in the constitution
+                    of India.
+                    Along with Assamese, it is geographically the most eastern of the Indo-Iranian languages.
+                    Like other Eastern Indo-Aryan languages, Bengali arose from the eastern Middle Indic
+                    languages of the Indian subcontinent. Rajbangsi, Kharia Thar and Mal Paharia are closely
+                    related to Western Bengali dialects, but are typically classified as separate languages. Similarly, Hajong is considered a separate language, although it shares similarities to Northern Bengali dialects.
                 </p>
             </div>
             <div>
                 <p>
-
+                    Like other Eastern Indo-Aryan languages, Bengali arose from the eastern Middle Indic
+                    languages of the Indian subcontinent. Magadhi Prakrit, the earliest recorded spoken
+                    language in the region and the language of the Buddha, had evolved into Ardhamagadhi
+                    ("Half Magadhi") in the early part of the first millennium C.E. Ardhamagadhi,
+                    as with all of the Prakrits of North India, began to give way to what are called
+                    Apabhramsa languages just before the turn of the first millennium.[3] The local
+                    Apabhramsa language of the eastern subcontinent, Purvi Apabhramsa or Apabhramsa Abahatta,
+                    eventually evolved into regional dialects, which in turn formed three groups: the Bihari
+                    languages, the Oriya languages, and the Bengali-Assamese languages. Some argue for much
+                    earlier points of divergence, going back to as early as 500 C.E.,[4] but the language was
+                    not static; different varieties coexisted and authors often wrote in multiple dialects.
+                    For example, Magadhi Prakrit is believed to have evolved into Apabhramsa Abahatta, which
+                    competed with Bengali for a period of time around the sixth century. There is general
+                    agreement that in the distant past Oriya, Assamese, and Bengali formed a single branch,
+                    from which Oriya split off first and Assamese later. This is one reason that the earliest
+                    specimens of Bengali language and literature, the Charyapadas (Buddhist mystic songs), are
+                    also claimed by speakers of Oriya and Assamese as their own.
                 </p>
             </div>
         </div>
