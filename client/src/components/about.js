c76b47462cdc9b1a455cd5a03b2708dabb178d50
khatrabiru <khatrabiru@gmail.com>
2022-01-13 19:11:40 +0000

    Add about page with social media contacts. Plus add some CSS effects.

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Social from './social'
 const Home = () => {
     return (
         <div className="container-fluid">
-            
-            
+            <div className="container4"> 
+            <p>
+                The languages spoken in Nepal, Bangladesh, India, Pakistan and Srilanka have so much in common because they come from same origin of Indo-Aryan Languages. Most of the words have same name and sound in most of those languages. I always wanted to know the meaning of particular thing and how they are being called in those languages. My main motto is to answer those curious minds who wants to research/study on Indo Aryan languages.
+                So far, I have managed to put 20 thousands of popular words in 8 different languages. If you want to contribute in translation of those words or if you think some translations are not perfect, please contact me on following social media links.
+            </p>
+            </div>
+            <Social/>
         </div>
     );
 
--- client/src/components/contact.js
+++ client/src/components/contact.js
@@ -2,14 +2,7 @@ import { useForm } from 'react-hook-form';
 import emailjs from 'emailjs-com';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
-import {
-  faYoutube,
-  faFacebook,
-  faTwitter,
-  faInstagram,
-  faLinkedin,
-} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Social from './social'
 
 const Contact = () => {
   const {
@@ -151,28 +144,7 @@ const Contact = () => {
               <ToastContainer />
             </div>
           </div>
-          <div class="social-container" align='center'>
-            <h3>Contact on Social Media</h3>
-            <a href="https://www.youtube.com/channel/UCbYIi9tIXN5fPKj0awCu4qw"
-              className="youtube social">
-              <FontAwesomeIcon icon={faYoutube} size="2x" />
-            </a>
-            <a href="https://www.facebook.com/biru.khatri"
-              className="facebook social">
-              <FontAwesomeIcon icon={faFacebook} size="2x" />
-            </a>
-            <a href="https://twitter.com/khatribru" className="twitter social">
-              <FontAwesomeIcon icon={faTwitter} size="2x" />
-            </a>
-            <a href="https://www.instagram.com/khatrabiru"
-              className="instagram social">
-              <FontAwesomeIcon icon={faInstagram} size="2x" />
-            </a>
-            <a href="https://www.linkedin.com/in/khatribiru/"
-              className="linkedin social">
-              <FontAwesomeIcon icon={faLinkedin} size="2x" />
-            </a>
-          </div>
+          <Social/>
         </div>
       </div>
     </div>
